ac6fb8977cc4eb4b42525595d749ccce473d4a3b
Vincenzo <vincenzo.mastromauro@edu.itspiemonte.it>
2022-07-08 03:20:16 +0200

    animation part1

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,10 +1,76 @@
 gsap.fromTo(
-  ".left h1",
-  { yPercent: 100 },
-  { yPercent: 0, duration: 3, ease: "power4.out" }
+  ".left h1 span",
+  { yPercent: 20, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 3, stagger: 0.2, ease: "power4.out" }
 );
-// gsap.fromTo(
-//   ".point span",
-//   { width: 0 },
-//   { width: 100, duration: 3, ease: "power4.out" }
-// );
+gsap.fromTo(
+  ".left a ",
+  { yPercent: 200, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 3, ease: "power4.out" }
+);
+gsap.fromTo(
+  ".right p ",
+  { yPercent: 200, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 5, ease: "power4.out" }
+);
+gsap.fromTo(
+  "aside",
+  { yPercent: -100, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 2, ease: "power4.out" }
+);
+gsap.fromTo(
+  "aside",
+  { yPercent: -100, opacity: 0 },
+  { yPercent: 0, opacity: 1, duration: 2, ease: "power4.out" }
+);
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".gallery",
+    markers: true,
+    start: "-20%",
+    end: "0%",
+  },
+});
+let tl2 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".categories",
+    markers: true,
+    start: "-20%",
+    end: "0%",
+  },
+});
+let tl3 = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".projectExplained",
+    markers: true,
+    start: "-20%",
+    end: "0%",
+  },
+});
+tl.addLabel("start")
+  .from(".gallery h3", { yPercent: -30, opacity: 0 })
+  .addLabel("end")
+  .to(".gallery h3", {
+    yPercent: 0,
+    opacity: 1,
+    duration: 9,
+  });
+tl2
+  .addLabel("start")
+  .from(".categories h3", { yPercent: -30, opacity: 0 })
+  .addLabel("end")
+  .to(".categories h3", {
+    yPercent: 0,
+    opacity: 1,
+    duration: 9,
+  });
+tl3
+  .addLabel("start")
+  .from(".projectExplained h3", { yPercent: -30, opacity: 0 })
+  .addLabel("end")
+  .to(".projectExplained h3", {
+    yPercent: 0,
+    opacity: 1,
+    duration: 9,
+  });
--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,15 @@
 let hamburger = document.getElementById("hamburger-menu");
 let navElement = document.getElementsByTagName("nav")[0];
 let dropdown = document.querySelector("nav ul li:nth-child(3) ol");
-let triggerForClosing = document.querySelector("nav ul:not(ol) ");
+let triggerForClosing = document.querySelector("nav ul:not(ol)");
 
 hamburger.addEventListener("click", () => {
   if (navElement.style.visibility === "hidden") {
     navElement.style.visibility = "visible";
     dropdown.style.display = "visible";
-    // document.body.style.overflowY = "hidden";
   } else {
     navElement.style.visibility = "hidden";
     dropdown.style.display = "hidden";
-    // document.body.style.overflowY = "scroll";
   }
 });
 
@@ -40,7 +38,7 @@ function showSlides() {
   }
   slides[slideIndex - 1].style.display = "flex";
   dots[slideIndex - 1].className += " active";
-  // setTimeout(showSlides, 4500); // Change image every 4.5 seconds
+  setTimeout(showSlides, 4500); // Change image every 4.5 seconds
 }
 // ETH - DOLL CONVERSION API
 var eth = document.getElementById("ethereumPrice");
